17a776116ca74796d8b0b94a3535c870b4050d6e
Katarina Durechova <katarina.durechova@nic.cz>
2014-07-28 17:10:10 +0200

    Add simple chattr command

--- kippo/commands/base.py
+++ kippo/commands/base.py
@@ -447,6 +447,19 @@ class command_init_iptables(HoneyPotCommand):
         self.writeln('bash: /etc/init.d/iptables: No such file or directory')
 commands['/etc/init.d/iptables'] = command_init_iptables
 
+class command_chattr(HoneyPotCommand):
+    def call(self):
+        if len(self.args) < 1:
+            self.writeln('Usage: chattr [-RVf] [-+=AacDdeijsSu] [-v version] files...')
+            return
+        elif len(self.args) < 2:
+            self.writeln("Must use '-v', =, - or +'")
+            return
+        if not self.fs.exists(self.args[1]):
+            self.writeln('chattr: No such file or directory while trying to stat ' + self.args[1])
+        return
+commands['/usr/bin/chattr'] = command_chattr
+
 class command_nop(HoneyPotCommand):
     def call(self):
         pass
