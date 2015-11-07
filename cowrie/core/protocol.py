a3ac14f814e46c79087cbec86ce1d7a2225bf74d
Michel Oosterhof <michel@oosterhof.net>
2015-11-07 19:59:27 +0000

    check we're still connected in writeln

--- cowrie/core/protocol.py
+++ cowrie/core/protocol.py
@@ -105,8 +105,10 @@ class HoneyPotBaseProtocol(insults.TerminalProtocol):
             self.cmdstack[-1].lineReceived(line)
 
     def writeln(self, data):
-        self.terminal.write(data)
-        self.terminal.nextLine()
+        # sometimes still called after disconnect because of a deferred
+        if self.terminal:
+            self.terminal.write(data)
+            self.terminal.nextLine()
 
     def call_command(self, cmd, *args):
         obj = cmd(self, *args)
