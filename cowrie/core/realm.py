ba210dacde4941eb1efb14af726571325c14d85c
Katarina Durechova <katarina.durechova@nic.cz>
2016-11-02 17:25:15 +0100

    Delete debug messages from realm

--- cowrie/core/realm.py
+++ cowrie/core/realm.py
@@ -50,14 +50,9 @@ class HoneyPotRealm:
     def requestAvatar(self, avatarId, mind, *interfaces):
 
         if mind in self.servers:
-            log.msg( "Using existing server for mind %s" % mind )
+            pass
         else:
-            log.msg( "Starting new server for mind %s" % mind )
             self.servers[mind] = server.CowrieServer(self.cfg)
-
-        for i in list(self.servers.keys()):
-            log.msg( "REFCOUNT: key: %s, refcount %d" % ( i, sys.getrefcount(self.servers[i])))
-            log.msg( "Refer: %s" % repr( gc.get_referrers(self.servers[i])))
 
         if conchinterfaces.IConchUser in interfaces:
             return interfaces[0], \
